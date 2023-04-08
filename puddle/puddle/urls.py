2bc8181dee2b78d7e8108158ce1f69d145147b60
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-04-08 11:36:46 +0300

    Fetch and display items on front page

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -21,10 +21,15 @@ from core.views import index
 from core.views import contact
 # from django.urls import include
 
+#Shouldn't be done in production!
+from django.conf import settings
+from django.conf.urls.static import static
+#Shouldn't be done in production!
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name="index"),
     path("contact/", contact, name="contact")
     # path('puddle/', include('idx')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
