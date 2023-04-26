ab6a6483225bbe7ad5e365029b944b194f22fcc2
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-04-26 22:27:34 +0300

    create edit item functionality

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -8,7 +8,7 @@ class NewItemForm(forms.ModelForm):
     
     class Meta:
         model = Item 
-        fields = ('category', 'name', 'description','price','image')
+        fields = ('category', 'name', 'description','price','image',)
         widgets = {
             'category':forms.Select(attrs={
                 'class':INPUT_CLASSES}),
@@ -27,4 +27,23 @@ class NewItemForm(forms.ModelForm):
             
             
             
+        }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            })
         }
