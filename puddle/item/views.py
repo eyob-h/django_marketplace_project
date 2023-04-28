f20e4ebab7098171f5a12084d91612b203043c2c
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-04-28 22:09:31 +0300

    create product filter

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -10,14 +10,20 @@ from .forms import NewItemForm, EditItemForm
 
 def items(request):
     query = request.GET.get('query','')
+    category_id = request.GET.get('category', 0 )
     items = Item.objects.filter(is_sold = False)
+    categories = Category.objects.all()
     
+    if category_id:
+        items = items.filter(category_id = category_id)
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
         
     return render(request, 'item/items.html', {
         'items': items,
         'query':query, 
+        'categories':categories,
+        'category_id':int(category_id),
     })
 
 def detail(request, pk):
